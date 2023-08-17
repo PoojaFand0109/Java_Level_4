24877e6ff335a39fd46d384b3f8fb5000803401f
PoojaFand0109 <141999392+PoojaFand0109@users.noreply.github.com>
2023-08-17 12:48:17 +0530

    Add files via upload

---
+++ Java_Level_4/src/com/level_4/LongestSubstring.java
@@ -0,0 +1,24 @@
+package com.level_4;
+
+public class LongestSubstring {
+
+	   public static void main(String[] args) {
+	        String sentence = "The quick brown fox jumps over the lazy dog.";
+	        String longestSubstring = findLongestSubstring(sentence);
+	        
+	        System.out.println("Longest substring: " + longestSubstring);
+	    }
+
+	    public static String findLongestSubstring(String sentence) {
+	        String[] words = sentence.split("\\s+");
+	        String longestSubstring = "";
+
+	        for (String word : words) {
+	            if (word.length() > longestSubstring.length()) {
+	                longestSubstring = word;
+	            }
+	        }
+
+	        return longestSubstring;
+	    }
+}
---
+++ Java_Level_4/src/com/level_4/RemoveWhiteSpaces.java
@@ -0,0 +1,15 @@
+package com.level_4;
+
+public class RemoveWhiteSpaces {
+
+	   public static void main(String[] args) {
+	        String input = "This is a sample string with white spaces.";
+	        String stringWithoutSpaces = removeWhiteSpaces(input);
+	        
+	        System.out.println("String without white spaces: " + stringWithoutSpaces);
+	    }
+
+	    public static String removeWhiteSpaces(String str) {
+	        return str.replaceAll("\\s+", "");
+	    }
+}
